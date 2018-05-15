6961f7628a70ed031804296ce7ea4990e406ffb0
Paris Yee <paris.yee@gmail.com>
2018-05-15 19:51:55 -0400

    first commit

---
+++ ninas/js/all.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+		$("a.video").fancybox(
+		{ 'hideOnContentClick': false,
+		'overlayShow'			: false,
+		'zoomOpacity'			: true,
+		'overlayShow'			: false,
+		'frameWidth'			:	720,
+		'frameHeight'			:	396,		
+		'zoomSpeedIn'			: 1000,
+		'zoomSpeedOut'			: 1000					
+		});
+});		
